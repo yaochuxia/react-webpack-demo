17a654a94a22d7de6f3f21f699ccf2499fefa7d7
yaochuxia <675993729@qq.com>
2018-01-15 16:56:39 +0800

    add app端

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Row,Col, Menu, Icon,Tabs,message,Form,Input,Button,CheckBox,Modal,Link } from 'antd';
+import {Row, Col,Menu,Icon,Tabs,message, Form,Input,Button,CheckBox,Modal} from 'antd';
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
+import {Router, Route, Link, browserHistory} from 'react-router'
 
-class PCHeader extends React.Component{
-    constructor(){
+class PCHeader extends React.Component {
+    constructor() {
         super();
         this.state = {
             current: 'top',
@@ -15,25 +16,37 @@ class PCHeader extends React.Component{
             hasLogined: false,
             userNickName: '',
             userid: 0
+        };
+    };
+
+    componentWillMount(){
+        if (localStorage.userid!='') {
+            this.setState({hasLogined:true});
+            this.setState({userNickName:localStorage.userNickName,userid:localStorage.userid});
         }
     };
-    setModalVisible(value){
+
+    setModalVisible(value)
+    {
         this.setState({modalVisible: value});
     };
-    handleClick(e){
-        if(e.key=='register'){
-            this.setState({current:'register'});
+    handleClick(e) {
+        if (e.key == "register") {
+            this.setState({current: 'register'});
             this.setModalVisible(true);
-        }else{
-            this.setState({current: e.key});
+        } else {
+            {
+                this.setState({current: e.key});
+            }
         }
     };
-    handleSubmit(e){
+    handleSubmit(e)
+    {
         //页面开始向 API 进行提交数据
         e.preventDefault();
         var myFetchOptions = {
             method: 'GET'
-        }
+        };
         var formData = this.props.form.getFieldsValue();
         console.log(formData);
         fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
@@ -41,90 +54,117 @@ class PCHeader extends React.Component{
         +"&r_userName=" + formData.r_userName + "&r_password="
         + formData.r_password + "&r_confirmPassword="
         + formData.r_confirmPassword, myFetchOptions)
-        .then(response=>response.json())
+        .then(response => response.json())
         .then(json => {
-            this.setState({userNickName:json.userNickName,userid:json.userid})
-        })
+            this.setState({userNickName: json.NickUserName, userid: json.UserId});
+            localStorage.userid= json.UserId;
+            localStorage.userNickName = json.NickUserName;
+        });
+        if (this.state.action=="login") {
+            this.setState({hasLogined:true});
+        }
         message.success("请求成功！");
         this.setModalVisible(false);
     };
-    render(){
-        let { getFieldProps } = this.props.form;
+    callback(key) {
+        if (key == 1) {
+            this.setState({action: 'login'});
+        } else if (key == 2) {
+            this.setState({action: 'register'});
+        }
+    };
+    logout(){
+        localStorage.userid= '';
+        localStorage.userNickName = '';
+        this.setState({hasLogined:false});
+    };
+    render() {
+        let {getFieldProps} = this.props.form;
         const userShow = this.state.hasLogined
-            ? <Menu.Item key='logout' className="register">
-                <Button type="primary" htmlType="button">{this.state.userNickName}</Button>  
-                &nbsp;&nbsp;
-                <Link target="_blank">
-                    <Button type="dashed" htmlType="button">个人中心</Button>
-                </Link>
-                &nbsp;&nbsp;
-                <Button type="ghost" htmlType="button">退出</Button>
-              </Menu.Item>
+            ? <Menu.Item key="logout" className="register">
+                    <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+                    &nbsp;&nbsp;
+                    <Link target="_blank">
+                        <Button type="dashed" htmlType="button">个人中心</Button>
+                    </Link>
+                    &nbsp;&nbsp;
+                    <Button type="ghost" htmlType="button" onClick={this.logout.bind(this)}>退出</Button>
+                </Menu.Item>
             : <Menu.Item key="register" className="register">
                 <Icon type="appstore"/>注册/登录
-              </Menu.Item>;
-        return(
+            </Menu.Item>;
+        return (
             <header>
                 <Row>
                     <Col span={1}></Col>
                     <Col span={4}>
                         <a href="/" className="logo">
-                            <img src="./src/images/logo.png"/>
+                            <img src="./src/images/logo.png" alt="logo"/>
                             <span>ReactNews</span>
                         </a>
                     </Col>
                     <Col span={18}>
-                        <Menu  mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
+                        <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
                             <Menu.Item key="top">
-                               <Icon type="appstore" /> 头条
+                                <Icon type="appstore"/>头条
                             </Menu.Item>
                             <Menu.Item key="shehui">
-                               <Icon type="appstore" /> 社会
+                                <Icon type="appstore"/>社会
                             </Menu.Item>
                             <Menu.Item key="guonei">
-                               <Icon type="appstore" /> 国内
+                                <Icon type="appstore"/>国内
                             </Menu.Item>
                             <Menu.Item key="guoji">
-                               <Icon type="appstore" /> 国际
+                                <Icon type="appstore"/>国际
                             </Menu.Item>
                             <Menu.Item key="yule">
-                               <Icon type="appstore" /> 娱乐
+                                <Icon type="appstore"/>娱乐
                             </Menu.Item>
                             <Menu.Item key="tiyu">
-                               <Icon type="appstore" /> 体育
+                                <Icon type="appstore"/>体育
                             </Menu.Item>
                             <Menu.Item key="keji">
-                               <Icon type="appstore" /> 科技
+                                <Icon type="appstore"/>科技
                             </Menu.Item>
                             <Menu.Item key="shishang">
-                               <Icon type="appstore" /> 时尚
+                                <Icon type="appstore"/>时尚
                             </Menu.Item>
                             {userShow}
                         </Menu>
-                        <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText="关闭">
-                            <Tabs type="card">
+                        <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText="关闭">
+                            <Tabs type="card" onChange={this.callback.bind(this)}>
+                                <TabPane tab="登录" key="1">
+                                    <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                                        <FormItem label="账户">
+                                            <Input placeholder="请输入您的账号" {...getFieldProps('userName')}/>
+                                        </FormItem>
+                                        <FormItem label="密码">
+                                            <Input type="password" placeholder="请输入您的密码" {...getFieldProps('password')}/>
+                                        </FormItem>
+                                        <Button type="primary" htmlType="submit">登录</Button>
+                                    </Form>
+                                </TabPane>
                                 <TabPane tab="注册" key="2">
                                     <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
                                         <FormItem label="账户">
-                                            <Input placeholder="请输入你的账号" {...getFieldProps('r_userName')} />
+                                            <Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
                                         </FormItem>
                                         <FormItem label="密码">
-                                            <Input type="password" placeholder="请输入你的密码" {...getFieldProps('r_password')} />
+                                            <Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
                                         </FormItem>
                                         <FormItem label="确认密码">
-                                            <Input type="password" placeholder="请再次输入你的密码" {...getFieldProps('r_comfirmPassword')} />
+                                            <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
                                         </FormItem>
-                                        <Button type="primary"htmlType="submit">注册</Button>
+                                        <Button type="primary" htmlType="submit">注册</Button>
                                     </Form>
                                 </TabPane>
-                                <TabPane tab="登录" key="1">Content of Tab Pane 2</TabPane>
                             </Tabs>
                         </Modal>
                     </Col>
-                    <Col span={1}></Col>  
+                    <Col span={1}></Col>
                 </Row>
             </header>
-        )
-    }
+        );
+    };
 }
 export default PCHeader = Form.create({})(PCHeader);
