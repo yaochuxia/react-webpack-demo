ca73bbbb6f826fba5e0b8a14f3709eedee85de8e
yaochuxia <675993729@qq.com>
2018-01-16 15:19:17 +0800

    app news

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
+import MobileList from './mobile_list';
 import {Tabs} from 'antd';
 const TabPane = Tabs.TabPane;
 export default class MobileIndex extends React.Component{
@@ -9,14 +10,30 @@ export default class MobileIndex extends React.Component{
             <div>
                 <MobileHeader/>
                     <Tabs >
-                        <TabPane tab="Tab 1" key="1">头条</TabPane>
-                        <TabPane tab="Tab 2" key="2">社会</TabPane>
-                        <TabPane tab="Tab 3" key="3">国内</TabPane>
-                        <TabPane tab="Tab 4" key="4">国际</TabPane>
-                        <TabPane tab="Tab 5" key="5">娱乐</TabPane>
-                        <TabPane tab="Tab 6" key="6">体育</TabPane>
-                        <TabPane tab="Tab 7" key="7">科技</TabPane>
-                        <TabPane tab="Tab 8" key="8">时尚</TabPane>
+                        <TabPane tab="头条" key="1">
+                            <MobileList count={15} type="toutiao"/>
+                        </TabPane>
+                        <TabPane tab="社会" key="2">
+                            <MobileList count={15} type="shehui"/>
+                        </TabPane>
+                        <TabPane tab="国内" key="3">
+                            <MobileList count={15} type="guonei"/>
+                        </TabPane>
+                        <TabPane tab="国际" key="4">
+                            <MobileList count={15} type="guoji"/>
+                        </TabPane>
+                        <TabPane tab="娱乐" key="5">
+                            <MobileList count={15} type="yule"/>
+                        </TabPane>
+                        <TabPane tab="体育" key="6">
+                            <MobileList count={15} type="tiyu"/>
+                        </TabPane>
+                        <TabPane tab="科技" key="7">
+                            <MobileList count={15} type="keji"/>
+                        </TabPane>
+                        <TabPane tab="时尚" key="8">
+                            <MobileList count={15} type="shishang"/>
+                        </TabPane>
                     </Tabs>
                 <MobileFooter/>
             </div>
--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -10,29 +10,33 @@ export default class PCNewsContainer extends React.Component{
         return(
             <div>
                 <Row>
-                    <Col span={2}></Col>
-                    <Col span={20} className="container">
+                    <Col span={1}></Col>
+                    <Col span={22} className="container">
                         <div className="leftContainer">
                             <Carousel autoplay>
-                                <div><img src="./src/images/carousel_1.jpg" width='400' height='350' /></div>
-                                <div><img src="./src/images/carousel_2.jpg" width='400' height='350' /></div>
-                                <div><img src="./src/images/carousel_3.jpg" width='400' height='350' /></div>
-                                <div><img src="./src/images/carousel_4.jpg" width='400' height='350' /></div>
+                                <div><img src="./src/images/carousel_1.jpg" width='370' height='370' /></div>
+                                <div><img src="./src/images/carousel_2.jpg" width='370' height='370' /></div>
+                                <div><img src="./src/images/carousel_3.jpg" width='370' height='370' /></div>
+                                <div><img src="./src/images/carousel_4.jpg" width='370' height='370' /></div>
                             </Carousel>
                         </div>
                         
                         <Tabs className="tabs_news">
                             <TabPane tab="新闻" key="1">
-                                <PCNewsBlock count={8} type="top" width='100%' bordered='false' /> 
+                                <PCNewsBlock count={15} type="top" width='370' bordered='false' /> 
                             </TabPane>
                             <TabPane tab="国际" key="2">
-                                <PCNewsBlock count={8} type="guoji" width='100%' bordered='false' /> 
+                                <PCNewsBlock count={15} type="guoji" width='370' bordered='false' /> 
                             </TabPane>
                         </Tabs> 
-                        <PCNewsImageBlock count={6} type="guoji" width="400px" cartTitle="国际头条" imageWidth="112px"/>                   
-                        <PCNewsImageBlock count={6} type="yule" width="400px" cartTitle="娱乐头条" imageWidth="112px"/>                   
+                        <PCNewsImageBlock count={6} type="guoji" width="370px" cartTitle="国际头条" imageWidth="112px"/>                   
+                        <PCNewsImageBlock count={6} type="yule" width="370px" cartTitle="娱乐头条" imageWidth="112px"/> 
+                        <div>
+                            <PCNewsImageBlock count={8} type="guonei" width="100%" cartTitle="国内新闻" imageWidth="132px"/>
+                            <PCNewsImageBlock count={16} type="yule" width="100%" cartTitle="娱乐新闻" imageWidth="132px"/>
+                        </div>                  
                     </Col>
-                    <Col span={2}></Col>
+                    <Col span={1}></Col>
                 </Row>
             </div>
         )
